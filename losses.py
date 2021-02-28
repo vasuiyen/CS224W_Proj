6b2f6396ab7e9ba5ac1b27ed598ee8b8fa5dda49
vasuiyen <61954526+vasuiyen@users.noreply.github.com>
2021-02-27 23:12:05 -0500

    Update losses.py

--- losses.py
+++ losses.py
@@ -6,4 +6,10 @@ def isometricLoss(output, target, type='L1'):
         loss = nn.L1Loss()
     if type == 'MSE':
         loss = nn.MSELoss()
+    if type == 'BCELogit':
+        loss = nn.BCEWithLogitsLoss()
+    if type == 'BCE':
+        loss = nn.BCELoss()
+    if type == 'NLL':
+        loss = nn.NLLLoss()
     return loss(output, target)
