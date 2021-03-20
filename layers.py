e0009816065624b07e74c2875cac5ee0dc41f6b8
vasuiyen <61954526+vasuiyen@users.noreply.github.com>
2021-03-20 14:06:37 -0400

    Update layers.py

--- layers.py
+++ layers.py
@@ -24,40 +24,32 @@ class ImplicitGraph(nn.Module):
     A Implicit Graph Neural Network Layer (IGNN)
     """
 
-    def __init__(self, in_features, out_features, num_node, kappa=0.99, init_type="uniform"):
+    def __init__(self, in_features, out_features, num_node, aggr, kappa=0.99):
         super(ImplicitGraph, self).__init__()
         self.p = in_features
         self.m = out_features
         self.n = num_node
-        self.k = kappa     
+        self.k = kappa 
+        self.aggr = aggr		
 
         self.W = nn.Parameter(torch.FloatTensor(self.m, self.m))
         self.Omega_1 = nn.Parameter(torch.FloatTensor(self.m, self.p))
         self.Omega_2 = nn.Parameter(torch.FloatTensor(self.m, self.p))
         self.bias = nn.Parameter(torch.FloatTensor(self.m, 1))
-        self.init(init_type)
+		
+        self.attn_layer = None
+        if self.aggr == "attn":
+           #self.attn_layer = nn.Sequential(nn.Linear(out_features, out_features, bias=True), nn.Tanh(), nn.Linear(out_features, 1, bias=False))
+           self.attn_layer = nn.Tanh()	   
+		   
+        self.init()
 
-    def init(self, init_type):
-
-        if init_type == 'uniform':
-            stdv = 1. / math.sqrt(self.W.size(1))
-            self.W.data.uniform_(-stdv, stdv)
-            self.Omega_1.data.uniform_(-stdv, stdv)
-            self.Omega_2.data.uniform_(-stdv, stdv)
-            self.bias.data.uniform_(-stdv, stdv)
-
-        elif init_type == 'kaiming_uniform':
-            torch.nn.init.kaiming_uniform_(self.W.data)
-            torch.nn.init.kaiming_uniform_(self.Omega_1.data)
-            torch.nn.init.kaiming_uniform_(self.Omega_2.data)
-            torch.nn.init.kaiming_uniform_(self.bias.data)
-
-        elif init_type == 'kaiming_normal':
-            torch.nn.init.kaiming_normal_(self.W.data)
-            torch.nn.init.kaiming_normal_(self.Omega_1.data)
-            torch.nn.init.kaiming_normal_(self.Omega_2.data)
-            torch.nn.init.kaiming_normal_(self.bias.data)
-
+    def init(self):
+        stdv = 1. / math.sqrt(self.W.size(1))
+        self.W.data.uniform_(-stdv, stdv)
+        self.Omega_1.data.uniform_(-stdv, stdv)
+        self.Omega_2.data.uniform_(-stdv, stdv)
+        self.bias.data.uniform_(-stdv, stdv)
 
     def forward(self, X_0, A, U, phi, A_rho=1.0, fw_mitr=300, bw_mitr=300):
         
@@ -65,6 +57,5 @@ class ImplicitGraph(nn.Module):
         support_1 = torch.spmm(torch.transpose(U, 0, 1), self.Omega_1.T).T
         support_1 = torch.spmm(torch.transpose(A, 0, 1), support_1.T).T
         
-        b_Omega = support_1
-        return ImplicitFunction.apply(self.W, X_0, A, b_Omega, phi, fw_mitr, bw_mitr)
-
+        b_Omega = support_1			
+        return ImplicitFunction.apply(self.W, X_0, A, b_Omega, phi, self.aggr, self.attn_layer, fw_mitr, bw_mitr)
